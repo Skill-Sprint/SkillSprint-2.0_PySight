92c5efca234d6e816a161318f74859edad6d0ee1
Aayush Chaudhary <chaudharyaayush230@gmail.com>
2024-01-22 18:29:37 +0530

    Task Submitted

---
+++ Class Projects/Assingment 1/Aayush_Chaudhary_2nd_MCA/facedetect/dataset.py
@@ -0,0 +1,34 @@
+import cv2
+import os
+import time
+haar_file="haarcascade_frontalface_default.xml"
+
+datasets="C:/Users/aayus/PycharmProjects/facedetect/dataset/"
+name ="Aayush"
+path=os.path.join(datasets,name)
+if not os.path.isdir(path):
+    os.mkdir(path)
+face_cascade=cv2.CascadeClassifier(haar_file)
+cap=cv2.VideoCapture(0)
+print("webcam is open?",cap.isOpened())
+time.sleep(2)
+count=1
+
+while count<=110:
+    ret,frame=cap.read()
+    if ret==True:
+        img_gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces=face_cascade.detectMultiScale(img_gray,1.3,4)
+        # detectMultiscale(source_image,scale,min_neighbours)
+        for(x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            face=img_gray[y:y+h,x:x+w]
+            face_resize=cv2.resize(face,(640,480))
+            cv2.imwrite("%s/%s.png"%(path,count),face_resize)
+        count=count+1
+        cv2.imshow("face capturing",frame)
+        if cv2.waitKey(1)==ord("q"):
+            break
+print("your face has been created")
+cap.release()
+cv2.destroyAllWindows()
