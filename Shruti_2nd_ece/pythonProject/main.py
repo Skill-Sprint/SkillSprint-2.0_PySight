038a4e2328fba916d366cf872744348955f1cef3
shrutisingh410 <143614018+shrutisingh410@users.noreply.github.com>
2024-01-28 20:00:10 +0530

    Task submitted

---
+++ Shruti_2nd_ece/pythonProject/main.py
@@ -0,0 +1,29 @@
+import cv2
+from ultralytics import YOLO
+
+# loding model
+model = YOLO('yolov8n.pt')
+
+# loading video
+#path = './test.mp4'
+
+# read frmaes
+cap = cv2.VideoCapture(0)
+ret = True
+while ret:
+    ret,frame = cap.read()
+    if ret==True:
+        # detecting objects from frame
+        # track object
+        result = model.track(frame, persist=True)
+
+        #plot results
+        frame_ = result[0].plot()
+
+        # visulaize
+        cv2.imshow('frame',frame_)
+        if cv2.waitKey(1)==ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
